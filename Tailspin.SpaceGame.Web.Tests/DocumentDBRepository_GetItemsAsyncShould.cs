3a0ccc264e0daf3e63456131cbc88cecf3348d23
= <ekin00@users.noreply.github.com>
2020-11-27 13:56:03 -0600

    try to failed a test

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -26,7 +26,7 @@ namespace Tests
 
         [TestCase("Milky Way")]
         [TestCase("Andromeda")]
-        [TestCase("Pinwheel")]
+        [TestCase("Pinwheel-BOB")]
         [TestCase("NGC 1300-BOB")]
         [TestCase("Messier 82")]
         public void FetchOnlyRequestedGameRegion(string gameRegion)
